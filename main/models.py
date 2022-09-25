c13b630e72e36b0746fe92aaa85595af75916a1b
Alamin Musa Magaga <55305080+alaminmagaga@users.noreply.github.com>
2022-09-24 19:46:51 -0700

    Update models.py

--- main/models.py
+++ main/models.py
@@ -29,7 +29,10 @@ class Scholarship(models.Model):
     def __str__(self):
         return self.title
 
-
+    @property
+    def image_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
 
 class Fellowship(models.Model):
     image=models.ImageField(null=True, blank=True, upload_to="images/")
