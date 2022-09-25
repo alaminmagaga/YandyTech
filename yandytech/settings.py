b80044bf812294715962848d83da4cc331d589da
alaminmagaga <alaminmusamagaga@gmail.com>
2022-09-24 17:34:09 -0700

    files

--- yandytech/settings.py
+++ yandytech/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-e68_p0cocmyqjm+ucvl(fa96aruln4li&ne1@-^-#sw#-(=y4x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yandytech.herokuapp.com','127.0.0.1']
 
 
 # Application definition
