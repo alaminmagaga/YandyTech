09ebd0cd8352b2b2652945d7dda0f05cae6e8f52
alaminmagaga <alaminmusamagaga@gmail.com>
2022-09-24 19:06:56 -0700

    files

--- yandytech/settings.py
+++ yandytech/settings.py
@@ -127,6 +127,9 @@ MEDIA_URL='/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
